5f552c249537f227e178db50c4e79852056c3a4a
anacarolinabmv <141560459+anacarolinabmv@users.noreply.github.com>
2024-07-25 12:02:33 +0200

    Fix relative path location

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 @app.route('/',methods=["GET","POST"])
 def index():
     if request.method=="POST":
-        pass
+        user = request.form["user_input"]
+        print(user)
     
     return render_template('index.html')
+
+if __name__=="__main__":
+    app.run(debug=True)
